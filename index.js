463ff12bd64d00cb1858902bca75bad9dffdbceb
Abdul Wahab <102620981+wahab315@users.noreply.github.com>
2023-08-27 03:15:53 +0500

    programming Task

---
+++ index.js
@@ -0,0 +1,76 @@
+///////////////////////////////
+// Write a program that can find the largest of the three numbers
+// let firstNum = 4;
+// let secondNum = 16;
+// let thirdNum = 10;
+// if (firstNum >= secondNum && firstNum >= thirdNum) {
+//   console.log("First Number is Greater with the value of " + firstNum);
+// } else if (secondNum >= firstNum && secondNum >= thirdNum) {
+//   console.log("Second Number is Greater with the value of " + secondNum);
+// } else {
+//   console.log("Third Number is Greater with the value of " + thirdNum);
+// }
+
+////////////////////////////////
+// Write a program to find the ratio of two numbers
+// let firstValue = 15;
+// let secondValue = 3;
+// console.log("Ratio of these two number is " + firstNum / secondNum);
+
+////////////////////////////////
+// Write a program that will print number 1 to 7 and their cubes separated by a tab using for loop.
+// for (let index = 1; index <= 7; index++) {
+//   const cubes = index * index * index;
+//   console.log(index + " cube " + cubes);
+// }
+
+////////////////////////////////
+// Write a program that prints the multiplication table of 12 from 1 to 10 using for loop
+// for (let index = 1; index <= 10; index++) {
+//   const anwer = 12 * index;
+//   console.log(`12 x ${index} = ${anwer}`);
+// }
+
+////////////////////////////////
+// Write a program that will take a number from user and will print its factorial at output. You must take care of negative number and zero as well
+// const inputNumber = 10;
+// const arr = [];
+// let result = 1;
+// if (inputNumber <= 0) {
+//   console.log("Please Enter Valid value here ");
+// }
+// for (let index = 1; index <= inputNumber; index++) {
+//   arr.push(index);
+// }
+// result = arr.reduce(
+//   (accumelator, currentValue) => accumelator * currentValue,
+//   result
+// );
+// console.log(result);
+
+////////////////////////////////
+// Write a program that that will take a number and if the number is even, it will print sum of all
+// number from 0 to that number and if the entered number is odd, it will print sum of all numbers from 1 to that number.
+// Furthermore, if user enters a negative number, it should display some message e.g., negative numbers are not allowed
+
+// const inputNumber = 15;
+// let value = 1;
+// const arr = [];
+
+// if (inputNumber < 0) {
+//   console.log("Error Please Enter Positive Value");
+// } else if (inputNumber % 2 == 0) {
+//   value = 0;
+// }
+
+// for (let index = value; index <= inputNumber; index++) {
+//   console.log(index);
+//   arr.push(index);
+// }
+
+// const result = arr.reduce(
+//   (accumelator, currentValue) => accumelator + currentValue,
+//   0
+// );
+
+// console.log(result);
